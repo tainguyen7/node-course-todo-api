a0112b97a644ef77edb3899f0c2b994ca4d42196
tainguyen7 <tnguye36@spsu.edu>
2017-08-02 13:49:50 -0400

    Add POST /todos route and refeactor mongoose

---
+++ server/models/user.js
@@ -0,0 +1,22 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+module.exports = {User};
+
+// var newUser = new User({
+//   email: 'bob@email'
+// });
+//
+// newUser.save().then((doc) => {
+//   console.log(JSON.stringify(doc, undefined, 2));
+// }, (e) => {
+//   console.log('Unable to save', e)
+// });
